fab2892181397db3a148a1babf0d7493076fa8aa
brennan.vanderlaan@gmail.com <brennan.vanderlaan@gmail.com>
2020-09-13 18:21:22 -0400

    Removing junk

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -12,7 +12,6 @@ void init(Plugin *p) {
 
 	p->addModel(modelRadioClient);
     p->addModel(modelRadioServer);
-	p->addModel(modelRadioBroadcast);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
